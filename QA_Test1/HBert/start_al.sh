6d45a6a73d9652f6c3c9483e6de292e8a4d45151
NicholasHsieh <hn14115262@smail.nchu.edu.tw>
2020-11-10 16:20:08 +0000

    QA_Test1/HBert/start_al.sh

--- QA_Test1/HBert/start_al.sh
+++ QA_Test1/HBert/start_al.sh
@@ -1,19 +1,24 @@
 CURRENT_DIR=`pwd`
-export DATA_DIR=$CURRENT_DIR/data
+export DATA_DIR=$CURRENT_DIR/dataR5
+export MODEL_DIR=$CURRENT_DIR/output_DRCD_albert
 
- ~/PycharmProjects/Pytorch/venv/Scripts/python run_squad.py \
+python run_squad.py \
   --model_type albert \
-  --model_name_or_path voidful/albert_chinese_tiny \
+  --model_name_or_path $MODEL_DIR \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --train_file $DATA_DIR/train_200.json \
-  --predict_file $DATA_DIR/test_200.json \
-  --per_gpu_train_batch_size 2 \
-  --per_gpu_eval_batch_size 2 \
+  --train_file $DATA_DIR/train_400.json \
+  --predict_file $DATA_DIR/Test_500.json \
+  --per_gpu_train_batch_size 6 \
+  --per_gpu_eval_batch_size 6 \
   --learning_rate 1e-5 \
-  --num_train_epochs 1.0 \
-  --max_seq_length 384 \
-  --doc_stride 128 \
-  --output_dir output_albert \
+  --num_train_epochs 2.0 \
+  --warmup_steps 1000 \
+  --max_seq_length 508 \
+  --max_query_length 2 \
+  --max_answer_length 508 \
+  --n_best_size 20 \
+  --doc_stride 192 \
+  --output_dir output_DRCD_albert_400_R5 \
   --save_steps 10000
