a10baa2169b4b7977745f796bfc016a12068c521
NicholasHsieh <hn14115262@smail.nchu.edu.tw>
2020-11-11 00:17:28 +0800

    .

--- QA_Test1/HBert/start_al.sh
+++ QA_Test1/HBert/start_al.sh
@@ -1,19 +1,23 @@
 CURRENT_DIR=`pwd`
-export DATA_DIR=$CURRENT_DIR/data
+export SQUAD_DIR=$CURRENT_DIR/dataR5
 
- ~/PycharmProjects/Pytorch/venv/Scripts/python run_squad.py \
-  --model_type albert \
-  --model_name_or_path voidful/albert_chinese_tiny \
+python run_squad.py \
+  --model_type bert \
+  --model_name_or_path bert-base-chinese \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --train_file $DATA_DIR/train_200.json \
-  --predict_file $DATA_DIR/test_200.json \
+  --train_file $SQUAD_DIR/train_200_X.json \
+  --predict_file $SQUAD_DIR/Test_500_X.json \
   --per_gpu_train_batch_size 2 \
   --per_gpu_eval_batch_size 2 \
   --learning_rate 1e-5 \
-  --num_train_epochs 1.0 \
-  --max_seq_length 384 \
-  --doc_stride 128 \
-  --output_dir output_albert \
+  --num_train_epochs 2.0 \
+  --warmup_steps 1000 \
+  --max_seq_length 508 \
+  --max_query_length 2 \
+  --max_answer_length 508 \
+  --n_best_size 20 \
+  --doc_stride 192 \
+  --output_dir output_bert_200_R5 \
   --save_steps 10000
